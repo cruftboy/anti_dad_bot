faf3ddcd512a243165d3c570dafc60ac93bc27c1
cruftboy <jasperhkw@gmail.com>
2017-11-16 16:39:38 -0800

    anti isaac support

--- Dad-Killer.py
+++ Dad-Killer.py
@@ -19,5 +19,16 @@ async def on_ready():
 async def on_message(message):
 	if(message.author.id == '247852652019318795'):
 		await client.delete_message(message)
+	elif(message.author.id == '261185713779900427'):
+		if('im' in message.content.lower()):
+			name = message.content.lower()
+			indexiam = name.find('im')
+			name = name[(indexiam+2):]
+			await client.send_message(message.channel, 'Hi' + name + ", i'm dad!")
+		elif("i'm" in message.content.lower()):
+			name = message.content.lower()
+			indexiam = name.find("i'm")
+			name = name[(indexiam+3):]
+			await client.send_message(message.channel, 'Hi' + name + ", i'm dad!")
 
 client.run('MzgwODQ3Nzg2OTc2ODcwNDEx.DO-lUw.MbS-5sLWySijrdfynLM1YfEgOgg')
