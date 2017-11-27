6948f4608a1f0ae34c60cf5c6f3cce251cf4c337
cruftboy <jasperhkw@gmail.com>
2017-11-27 15:23:53 -0800

    bad bot

--- Dad-Killer.py
+++ Dad-Killer.py
@@ -11,24 +11,21 @@ async def on_ready():
 	print(client.user.name)
 	print(client.user.id)
 	print('------')
-	await client.change_presence(game=discord.Game(name="try d!help"))
+	await client.change_presence(game=discord.Game(name="your mom"))
 
 
 
 @client.event
 async def on_message(message):
-	if(message.author.id == '247852652019318795'):
-		await client.delete_message(message)
-	elif(message.author.id == '261185713779900427'):
-		if('im' in message.content.lower()):
-			name = message.content.lower()
-			indexiam = name.find('im')
-			name = name[(indexiam+2):]
-			await client.send_message(message.channel, 'Hi' + name + ", i'm dad!")
-		elif("i'm" in message.content.lower()):
-			name = message.content.lower()
-			indexiam = name.find("i'm")
-			name = name[(indexiam+3):]
-			await client.send_message(message.channel, 'Hi' + name + ", i'm dad!")
+
+	messageType = random.randrange(0,3)
+
+	if(message.author.id != client.user.id):
+		if(messageType == 0):
+			await client.send_message(message.channel, 'You were an accident ' + message.author.display_name)
+		elif(messageType == 1):
+			await client.send_message(message.channel, 'Call me Bob Ross on anticonvulsants, because I just found a sad little accident')
+		elif(messageType == 2):
+			await client.send_message(message.channel, 'You are in the 0.01 percent, because with you, the condom broke')
 
 client.run('MzgwODQ3Nzg2OTc2ODcwNDEx.DO-lUw.MbS-5sLWySijrdfynLM1YfEgOgg')
